e187ed45e14e12f3dce9e1a09e4b6347c15aca99
Jason Zhekov <jasssonpet@gmail.com>
2016-08-01 14:59:05 +0300

    Clang-format 3.9

---
+++ src/NativeScript/inspector/SourceProviderManager.cpp
@@ -0,0 +1,25 @@
+#include "SourceProviderManager.h"
+#include "inspector/CachedResource.h"
+#include <JavaScriptCore/inspector/ScriptDebugListener.h>
+#include <stdio.h>
+
+namespace NativeScript {
+RefPtr<JSC::SourceProvider> ResourceManager::addSourceProvider(WTF::String url, WTF::String moduleBody) {
+    WTF::String functionContent = constructFunctionContent(moduleBody);
+    WTF::TextPosition startPosition;
+    RefPtr<JSC::SourceProvider> sourceProvider = JSC::StringSourceProvider::create(functionContent, url, startPosition);
+
+    m_sourceProviders.add(url, sourceProvider);
+
+    return sourceProvider;
+}
+
+WTF::String ResourceManager::constructFunctionContent(WTF::String moduleBody) {
+    StringBuilder builder;
+    builder.append("{function anonymous(require, module, exports, __dirname, __filename) {");
+    builder.append(moduleBody);
+    builder.append("\n}}");
+
+    return builder.toString();
+}
+}
